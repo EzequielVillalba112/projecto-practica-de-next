279f0c507645975c2d17e508fe7c940f803b9cca
Ezequiel Villalba <148280303+EzequielVillalba112@users.noreply.github.com>
2026-02-13 18:55:31 -0300

    First Commit

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@radix-ui/themes";
+import { BookmarkIcon } from "@radix-ui/react-icons";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <Button>
+        <BookmarkIcon /> Bookmark
+      </Button>
+    </div>
+  );
+};
+
+export default Login;
